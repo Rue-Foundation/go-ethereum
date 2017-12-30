040f93d2482c3ddf33d47ee0d6586664e0663f36
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 16:27:26 -0500

    Update ens.go

--- contracts/ens/contract/ens.go
+++ contracts/ens/contract/ens.go
@@ -6,10 +6,10 @@ package contract
 import (
 	"strings"
 
-	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/Rue-Foundation/go-rue/accounts/abi"
+	"github.com/Rue-Foundation/go-rue/accounts/abi/bind"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/core/types"
 )
 
 // ENSABI is the input ABI used to generate the binding from.
