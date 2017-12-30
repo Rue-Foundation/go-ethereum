f80ea879a50c14a328bae62c3dbc70719d68fa80
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 16:53:08 -0500

    Update gas_table.go

--- core/vm/gas_table.go
+++ core/vm/gas_table.go
@@ -19,9 +19,9 @@ package vm
 import (
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/math"
-	"github.com/ethereum/go-ethereum/params"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/common/math"
+	"github.com/Rue-Foundation/go-rue/params"
 )
 
 // memoryGasCosts calculates the quadratic gas for memory expansion. It does so
