c06fbe2f6ff7988162b794ca99f0fb619466b301
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 10:06:50 -0500

    Update keystore_test.go

--- accounts/keystore/keystore_test.go
+++ accounts/keystore/keystore_test.go
@@ -26,9 +26,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethereum/go-ethereum/accounts"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/event"
+	"github.com/Rue-Foundation/go-rue/accounts"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/event"
 )
 
 var testSigData = make([]byte, 32)
