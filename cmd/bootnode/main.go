9fbc91be3485c93f1de2bbebcfbf2a7827f9a7ec
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 13:10:12 -0500

    Update main.go

--- cmd/bootnode/main.go
+++ cmd/bootnode/main.go
@@ -23,13 +23,13 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/ethereum/go-ethereum/cmd/utils"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/p2p/discover"
-	"github.com/ethereum/go-ethereum/p2p/discv5"
-	"github.com/ethereum/go-ethereum/p2p/nat"
-	"github.com/ethereum/go-ethereum/p2p/netutil"
+	"github.com/Rue-Foundation/go-rue/cmd/utils"
+	"github.com/Rue-Foundation/go-rue/crypto"
+	"github.com/Rue-Foundation/go-rue/log"
+	"github.com/Rue-Foundation/go-rue/p2p/discover"
+	"github.com/Rue-Foundation/go-rue/p2p/discv5"
+	"github.com/Rue-Foundation/go-rue/p2p/nat"
+	"github.com/Rue-Foundation/go-rue/p2p/netutil"
 )
 
 func main() {
