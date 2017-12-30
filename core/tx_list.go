b527607b7c04a82d03a82c6dbf916d770cc8ebd3
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 17:12:56 -0500

    Update tx_list.go

--- core/tx_list.go
+++ core/tx_list.go
@@ -22,9 +22,9 @@ import (
 	"math/big"
 	"sort"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/core/types"
+	"github.com/Rue-Foundation/go-rue/log"
 )
 
 // nonceHeap is a heap.Interface implementation over 64bit unsigned integers for
