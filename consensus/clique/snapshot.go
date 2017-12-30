2575c7bd3ffb3334da617658e47f17a633ecb076
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 15:38:18 -0500

    Update snapshot.go

--- consensus/clique/snapshot.go
+++ consensus/clique/snapshot.go
@@ -20,10 +20,10 @@ import (
 	"bytes"
 	"encoding/json"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/ethdb"
-	"github.com/ethereum/go-ethereum/params"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/core/types"
+	"github.com/Rue-Foundation/go-rue/ethdb"
+	"github.com/Rue-Foundation/go-rue/params"
 	lru "github.com/hashicorp/golang-lru"
 )
 
