3db2f91b8907e0c1a70113001cddcffe48b54a6d
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 10:08:25 -0500

    Update presale.go

--- accounts/keystore/presale.go
+++ accounts/keystore/presale.go
@@ -25,8 +25,8 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/ethereum/go-ethereum/accounts"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/Rue-Foundation/go-rue/accounts"
+	"github.com/Rue-Foundation/go-rue/crypto"
 	"github.com/pborman/uuid"
 	"golang.org/x/crypto/pbkdf2"
 )
