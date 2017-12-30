36a9e98bb473fb38a86c7f966774cdf52566de1b
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 15:20:46 -0500

    Update config.go

--- cmd/swarm/config.go
+++ cmd/swarm/config.go
@@ -27,13 +27,13 @@ import (
 
 	cli "gopkg.in/urfave/cli.v1"
 
-	"github.com/ethereum/go-ethereum/cmd/utils"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/node"
+	"github.com/Rue-Foundation/go-rue//cmd/utils"
+	"github.com/Rue-Foundation/go-rue//common"
+	"github.com/Rue-Foundation/go-rue//log"
+	"github.com/Rue-Foundation/go-rue//node"
 	"github.com/naoina/toml"
 
-	bzzapi "github.com/ethereum/go-ethereum/swarm/api"
+	bzzapi "github.com/Rue-Foundation/go-rue//swarm/api"
 )
 
 var (
@@ -83,7 +83,7 @@ var tomlSettings = toml.Config{
 	MissingField: func(rt reflect.Type, field string) error {
 		link := ""
 		if unicode.IsUpper(rune(rt.Name()[0])) && rt.PkgPath() != "main" {
-			link = fmt.Sprintf(", check github.com/ethereum/go-ethereum/swarm/api/config.go for available fields")
+			link = fmt.Sprintf(", check github.com/Rue-Foundation/go-rue/swarm/api/config.go for available fields")
 		}
 		return fmt.Errorf("field '%s' is not defined in %s%s", field, rt.String(), link)
 	},
