e69baa436e43a6e87ee80ce5297dcb5a531d2aea
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 16:39:39 -0500

    Update database.go

--- core/state/database.go
+++ core/state/database.go
@@ -20,9 +20,9 @@ import (
 	"fmt"
 	"sync"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/ethdb"
-	"github.com/ethereum/go-ethereum/trie"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/ethdb"
+	"github.com/Rue-Foundation/go-rue/trie"
 	lru "github.com/hashicorp/golang-lru"
 )
 
