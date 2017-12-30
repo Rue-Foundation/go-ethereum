0443a1c010846b8d22189c8a8f4f3addba144fd4
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 16:45:56 -0500

    Update log.go

--- core/types/log.go
+++ core/types/log.go
@@ -20,9 +20,9 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/common/hexutil"
+	"github.com/Rue-Foundation/go-rue/rlp"
 )
 
 //go:generate gencodec -type Log -field-override logMarshaling -out gen_log_json.go
