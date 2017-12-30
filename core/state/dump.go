59f9eb42fc46ec4927a24e7cff941b4d08eff556
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 16:39:55 -0500

    Update dump.go

--- core/state/dump.go
+++ core/state/dump.go
@@ -20,9 +20,9 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/rlp"
-	"github.com/ethereum/go-ethereum/trie"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/rlp"
+	"github.com/Rue-Foundation/go-rue/trie"
 )
 
 type DumpAccount struct {
