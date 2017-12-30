e158254a331c78a84a8b1c29af37594a6c09b100
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 16:49:07 -0500

    Update env.go

--- core/vm/runtime/env.go
+++ core/vm/runtime/env.go
@@ -19,9 +19,9 @@ package runtime
 import (
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/core"
+	"github.com/Rue-Foundation/go-rue/core/vm"
 )
 
 func NewEnv(cfg *Config) *vm.EVM {
