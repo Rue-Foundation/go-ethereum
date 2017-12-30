6d6f0317886a33c2db07c36fe5f18892f4bdd971
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 13:30:42 -0500

    Update run_test.go

--- cmd/geth/run_test.go
+++ cmd/geth/run_test.go
@@ -23,11 +23,11 @@ import (
 	"testing"
 
 	"github.com/docker/docker/pkg/reexec"
-	"github.com/ethereum/go-ethereum/internal/cmdtest"
+	"github.com/Rue-Foundation/go-rue/internal/cmdtest"
 )
 
 func tmpdir(t *testing.T) string {
-	dir, err := ioutil.TempDir("", "geth-test")
+	dir, err := ioutil.TempDir("", "grue-test")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -44,7 +44,7 @@ type testgeth struct {
 
 func init() {
 	// Run the app if we've been exec'd as "geth-test" in runGeth.
-	reexec.Register("geth-test", func() {
+	reexec.Register("grue-test", func() {
 		if err := app.Run(os.Args); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
@@ -92,7 +92,7 @@ func runGeth(t *testing.T, args ...string) *testgeth {
 
 	// Boot "geth". This actually runs the test binary but the TestMain
 	// function will prevent any tests from running.
-	tt.Run("geth-test", args...)
+	tt.Run("grue-test", args...)
 
 	return tt
 }
