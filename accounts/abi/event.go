ddcccaca099ffcfc4c26e5b21067d64caadeeafd
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 09:55:10 -0500

    Update event.go

--- accounts/abi/event.go
+++ accounts/abi/event.go
@@ -20,8 +20,8 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/crypto"
 )
 
 // Event is an event potentially triggered by the EVM's LOG mechanism. The Event
