f071a6f6a666abd088f77bc65412122365078bb3
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 15:36:04 -0500

    Update types.go

--- common/types.go
+++ common/types.go
@@ -23,8 +23,8 @@ import (
 	"math/rand"
 	"reflect"
 
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/crypto/sha3"
+	"github.com/Rue-Foundation/go-rue/common/hexutil"
+	"github.com/Rue-Foundation/go-rue/crypto/sha3"
 )
 
 const (
