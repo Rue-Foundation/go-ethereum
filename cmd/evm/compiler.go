9c1863908f47aaf28b7f5022d3dc3864105131a9
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 13:11:30 -0500

    Update compiler.go

--- cmd/evm/compiler.go
+++ cmd/evm/compiler.go
@@ -21,7 +21,7 @@ import (
 	"fmt"
 	"io/ioutil"
 
-	"github.com/ethereum/go-ethereum/cmd/evm/internal/compiler"
+	"github.com/Rue-Foundation/go-rue/cmd/evm/internal/compiler"
 
 	cli "gopkg.in/urfave/cli.v1"
 )
