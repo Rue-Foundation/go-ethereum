1dcc6b14d4fe30f32198f796b7c9f6282ae4c735
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 16:50:41 -0500

    Update common.go

--- core/vm/common.go
+++ core/vm/common.go
@@ -19,8 +19,8 @@ package vm
 import (
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/math"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/common/math"
 )
 
 // calculates the memory size required for a step
