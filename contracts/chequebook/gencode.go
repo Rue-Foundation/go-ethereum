9edfe2547168b4d16ded5f0c1eafe9ac0872d1d3
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 16:25:04 -0500

    Update gencode.go

--- contracts/chequebook/gencode.go
+++ contracts/chequebook/gencode.go
@@ -25,11 +25,11 @@ import (
 	"io/ioutil"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
-	"github.com/ethereum/go-ethereum/contracts/chequebook/contract"
-	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/Rue-Foundation/go-rue/accounts/abi/bind"
+	"github.com/eRue-Foundation/go-rue/accounts/abi/bind/backends"
+	"github.com/Rue-Foundation/go-rue/contracts/chequebook/contract"
+	"github.com/Rue-Foundation/go-rue/core"
+	"github.com/Rue-Foundation/go-rue/crypto"
 )
 
 var (
