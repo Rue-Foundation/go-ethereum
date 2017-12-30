79d324468852ce0b9828c83d6816d6229daee566
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 13:14:39 -0500

    Update staterunner.go

--- cmd/evm/staterunner.go
+++ cmd/evm/staterunner.go
@@ -23,10 +23,10 @@ import (
 	"io/ioutil"
 	"os"
 
-	"github.com/ethereum/go-ethereum/core/state"
-	"github.com/ethereum/go-ethereum/core/vm"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/tests"
+	"github.com/Rue-Foundation/go-rue/core/state"
+	"github.com/Rue-Foundation/go-rue/core/vm"
+	"github.com/Rue-Foundation/go-rue/log"
+	"github.com/Rue-Foundation/go-rue/tests"
 
 	cli "gopkg.in/urfave/cli.v1"
 )
