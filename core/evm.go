902f17b5034d8d9eed6ec14fe976bc1754c087a2
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 17:05:04 -0500

    Update evm.go

--- core/evm.go
+++ core/evm.go
@@ -19,10 +19,10 @@ package core
 import (
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/consensus"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/consensus"
+	"github.com/Rue-Foundation/go-rue/core/types"
+	"github.com/Rue-Foundation/go-rue/core/vm"
 )
 
 // ChainContext supports retrieving headers and consensus parameters from the
