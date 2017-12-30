1d370d2543b9fdbecf4cbf8f2ce5fcc060004849
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 17:12:28 -0500

    Update tx_journal.go

--- core/tx_journal.go
+++ core/tx_journal.go
@@ -21,10 +21,10 @@ import (
 	"io"
 	"os"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/core/types"
+	"github.com/Rue-Foundation/go-rue/log"
+	"github.com/Rue-Foundation/go-rue/rlp"
 )
 
 // errNoActiveJournal is returned if a transaction is attempted to be inserted
