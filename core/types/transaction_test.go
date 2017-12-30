82330bee5961ed78979929d995beca6a996a39e4
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 16:48:38 -0500

    Update transaction_test.go

--- core/types/transaction_test.go
+++ core/types/transaction_test.go
@@ -23,9 +23,9 @@ import (
 	"math/big"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/crypto"
+	"github.com/Rue-Foundation/go-rue/rlp"
 )
 
 // The values in those tests are from the Transaction Tests
