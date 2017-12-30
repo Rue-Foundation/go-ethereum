c643cedcb73ad15b9c7732cfe8fecd26ddbdfdcc
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 17:13:39 -0500

    Update tx_pool.go

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -25,13 +25,13 @@ import (
 	"sync"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/state"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/event"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/metrics"
-	"github.com/ethereum/go-ethereum/params"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/core/state"
+	"github.com/Rue-Foundation/go-rue/core/types"
+	"github.com/Rue-Foundation/go-rue/event"
+	"github.com/Rue-Foundation/go-rue/log"
+	"github.com/Rue-Foundation/go-rue/metrics"
+	"github.com/Rue-Foundation/go-rue/params"
 	"gopkg.in/karalabe/cookiejar.v2/collections/prque"
 )
 
