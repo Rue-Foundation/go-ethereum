ea4f5785489921bfcdc6a04ca6da9320b2e335ea
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 16:53:41 -0500

    Update gen_structlog.go

--- core/vm/gen_structlog.go
+++ core/vm/gen_structlog.go
@@ -6,9 +6,9 @@ import (
 	"encoding/json"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/common/math"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/common/hexutil"
+	"github.com/Rue-Foundation/go-rue/common/math"
 )
 
 func (s StructLog) MarshalJSON() ([]byte, error) {
