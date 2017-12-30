d667487f98a3c04b41a97da08c0619ddea5e0b9b
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 16:57:08 -0500

    Update logger.go

--- core/vm/logger.go
+++ core/vm/logger.go
@@ -23,10 +23,10 @@ import (
 	"math/big"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/common/math"
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/common/hexutil"
+	"github.com/Rue-Foundation/go-rue/common/math"
+	"github.com/Rue-Foundation/go-rue/core/types"
 )
 
 type Storage map[common.Hash]common.Hash
