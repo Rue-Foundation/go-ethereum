5619baaa1afe2c341d357a02d2ef235eee810307
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 16:21:52 -0500

    Update chequebook.go

--- contracts/chequebook/contract/chequebook.go
+++ contracts/chequebook/contract/chequebook.go
@@ -7,10 +7,10 @@ import (
 	"math/big"
 	"strings"
 
-	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/Rue-Foundation/go-rue/accounts/abi"
+	"github.com/Rue-Foundation/go-rue/accounts/abi/bind"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/core/types"
 )
 
 // ChequebookABI is the input ABI used to generate the binding from.
