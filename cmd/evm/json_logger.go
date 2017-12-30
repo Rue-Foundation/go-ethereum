acaf62bff55f778793150bb368f11a3a02e8983e
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 13:12:44 -0500

    Update json_logger.go

--- cmd/evm/json_logger.go
+++ cmd/evm/json_logger.go
@@ -22,9 +22,9 @@ import (
 	"math/big"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/math"
-	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/common/math"
+	"github.com/Rue-Foundation/go-rue/core/vm"
 )
 
 type JSONLogger struct {
