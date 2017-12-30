5241af80e761eb0640ab2b84ce603418fec413a1
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 16:11:49 -0500

    Update sealer.go

--- consensus/ethash/sealer.go
+++ consensus/ethash/sealer.go
@@ -24,10 +24,10 @@ import (
 	"runtime"
 	"sync"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/consensus"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/consensus"
+	"github.com/Rue-Foundation/go-rue/core/types"
+	"github.com/Rue-Foundation/go-rue/log"
 )
 
 // Seal implements consensus.Engine, attempting to find a nonce that satisfies
