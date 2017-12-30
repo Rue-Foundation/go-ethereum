a6e1a579729d3711fa9b0b83bf40ebcfd379a2be
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 16:54:55 -0500

    Update interface.go

--- core/vm/interface.go
+++ core/vm/interface.go
@@ -19,8 +19,8 @@ package vm
 import (
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/core/types"
 )
 
 // StateDB is an EVM database for full state querying.
