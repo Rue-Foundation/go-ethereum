53364a8bf6012b1189f89c19b6012f645ee53460
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 10:02:19 -0500

    Update account_cache_test.go

--- accounts/keystore/account_cache_test.go
+++ accounts/keystore/account_cache_test.go
@@ -29,8 +29,8 @@ import (
 
 	"github.com/cespare/cp"
 	"github.com/davecgh/go-spew/spew"
-	"github.com/ethereum/go-ethereum/accounts"
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/Rue-Foundation/go-rue/accounts"
+	"github.com/Rue-Foundation/go-rue/common"
 )
 
 var (
