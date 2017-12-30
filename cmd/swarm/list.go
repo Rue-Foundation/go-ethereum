d8cb777feebc0d51ebc8afe133ad867a79e2225b
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 15:22:37 -0500

    Update list.go

--- cmd/swarm/list.go
+++ cmd/swarm/list.go
@@ -22,8 +22,8 @@ import (
 	"strings"
 	"text/tabwriter"
 
-	"github.com/ethereum/go-ethereum/cmd/utils"
-	swarm "github.com/ethereum/go-ethereum/swarm/api/client"
+	"github.com/Rue-Foundation/go-rue/cmd/utils"
+	swarm "github.com/Rue-Foundation/go-rue/swarm/api/client"
 	"gopkg.in/urfave/cli.v1"
 )
 
