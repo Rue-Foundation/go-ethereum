a94a330783ea9051615849f42dd02a618f72e375
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 17:04:40 -0500

    Update events.go

--- core/events.go
+++ core/events.go
@@ -17,8 +17,8 @@
 package core
 
 import (
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/core/types"
 )
 
 // TxPreEvent is posted when a transaction enters the transaction pool.
