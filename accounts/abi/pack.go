f499d41b8e027d9d724b938d356a5f250d2bd4d8
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 09:56:51 -0500

    Update pack.go

--- accounts/abi/pack.go
+++ accounts/abi/pack.go
@@ -20,8 +20,8 @@ import (
 	"math/big"
 	"reflect"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/math"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/common/math"
 )
 
 // packBytesSlice packs the given bytes as [L, V] as the canonical representation
