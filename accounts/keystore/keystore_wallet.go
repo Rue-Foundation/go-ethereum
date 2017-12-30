2a3137aa0376da2f33f07ee7e604a7a91ed720d5
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 10:08:01 -0500

    Update keystore_wallet.go

--- accounts/keystore/keystore_wallet.go
+++ accounts/keystore/keystore_wallet.go
@@ -19,9 +19,9 @@ package keystore
 import (
 	"math/big"
 
-	ethereum "github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/accounts"
-	"github.com/ethereum/go-ethereum/core/types"
+	rue "github.com/Rue-Foundation/go-rue"
+	"github.com/Rue-Foundation/go-rue/accounts"
+	"github.com/Rue-Foundation/go-rue/core/types"
 )
 
 // keystoreWallet implements the accounts.Wallet interface for the original
