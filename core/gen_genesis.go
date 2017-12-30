fedf989398dddbf8c3abe59034ef49f45622e309
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 17:06:31 -0500

    Update gen_genesis.go

--- core/gen_genesis.go
+++ core/gen_genesis.go
@@ -7,10 +7,10 @@ import (
 	"errors"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/common/math"
-	"github.com/ethereum/go-ethereum/params"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/common/hexutil"
+	"github.com/Rue-Foundation/go-rue/common/math"
+	"github.com/Rue-Foundation/go-rue/params"
 )
 
 func (g Genesis) MarshalJSON() ([]byte, error) {
