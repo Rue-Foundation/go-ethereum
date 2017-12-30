4b1df46eded42b7db379c19247f848f7ad81603e
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 10:04:22 -0500

    Update keystore.go

--- accounts/keystore/keystore.go
+++ accounts/keystore/keystore.go
@@ -33,11 +33,11 @@ import (
 	"sync"
 	"time"
 
-	"github.com/ethereum/go-ethereum/accounts"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/event"
+	"github.com/Rue-Foundation/go-rue/accounts"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/core/types"
+	"github.com/Rue-Foundation/go-rue/crypto"
+	"github.com/Rue-Foundation/go-rue/event"
 )
 
 var (
