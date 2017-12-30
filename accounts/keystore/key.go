8569d429a3a5ede86459a7b5c2d32a7ae940d1a0
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 10:03:19 -0500

    Update key.go

--- accounts/keystore/key.go
+++ accounts/keystore/key.go
@@ -29,9 +29,9 @@ import (
 	"strings"
 	"time"
 
-	"github.com/ethereum/go-ethereum/accounts"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/Rue-Foundation/go-rue/accounts"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/crypto"
 	"github.com/pborman/uuid"
 )
 
