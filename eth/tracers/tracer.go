37fb2e06be14411fa0cb581de18d66a44b535702
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 20:15:00 -0500

    Update tracer.go

--- eth/tracers/tracer.go
+++ eth/tracers/tracer.go
@@ -25,11 +25,11 @@ import (
 	"time"
 	"unsafe"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/core/vm"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/common/hexutil"
+	"github.com/Rue-Foundation/go-rue/core/vm"
+	"github.com/Rue-Foundation/go-rue/crypto"
+	"github.com/Rue-Foundation/go-rue/log"
 	duktape "gopkg.in/olebedev/go-duktape.v3"
 )
 
