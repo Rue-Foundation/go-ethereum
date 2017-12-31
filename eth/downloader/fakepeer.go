2848fab81e57d995500acb3c0e922516574f22ff
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 20:06:25 -0500

    Update fakepeer.go

--- eth/downloader/fakepeer.go
+++ eth/downloader/fakepeer.go
@@ -19,10 +19,10 @@ package downloader
 import (
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/ethdb"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/core"
+	"github.com/Rue-Foundation/go-rue/types"
+	"github.com/Rue-Foundation/go-rue/ethdb"
 )
 
 // FakePeer is a mock downloader peer that operates on a local database instance
