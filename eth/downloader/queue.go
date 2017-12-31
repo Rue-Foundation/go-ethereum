f0b4b108f96f3eb890c13a40283e5831980996f6
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 20:07:54 -0500

    Update queue.go

--- eth/downloader/queue.go
+++ eth/downloader/queue.go
@@ -25,9 +25,9 @@ import (
 	"sync"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/core/types"
+	"github.com/Rue-Foundation/go-rue/log"
 	"github.com/rcrowley/go-metrics"
 	"gopkg.in/karalabe/cookiejar.v2/collections/prque"
 )
