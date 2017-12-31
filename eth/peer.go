946f850eb3c59578e39dad1e0252ac6f3546834f
rue-network <34838535+rue-network@users.noreply.github.com>
2017-12-30 20:28:08 -0500

    Update peer.go

--- eth/peer.go
+++ eth/peer.go
@@ -23,10 +23,10 @@ import (
 	"sync"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/p2p"
-	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/core/types"
+	"github.com/Rue-Foundation/go-rue/p2p"
+	"github.com/Rue-Foundation/go-rue/rlp"
 	"gopkg.in/fatih/set.v0"
 )
 
