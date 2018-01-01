83544e2731054d10bf3b5b48db0cb0f2aa5460c6
rue-network <34838535+rue-network@users.noreply.github.com>
2018-01-01 09:31:24 -0500

    Update hasher.go

--- trie/hasher.go
+++ trie/hasher.go
@@ -21,9 +21,9 @@ import (
 	"hash"
 	"sync"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto/sha3"
-	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/crypto/sha3"
+	"github.com/Rue-Foundation/go-rue/rlp"
 )
 
 // calculator is a utility used by the hasher to calculate the hash value of the tree node.
