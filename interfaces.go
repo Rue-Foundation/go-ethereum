46ebaa5a6f3532333857ae4513aae1403ba06aa9
rue-network <34838535+rue-network@users.noreply.github.com>
2018-01-01 11:50:54 -0500

    Update interfaces.go

--- interfaces.go
+++ interfaces.go
@@ -22,8 +22,8 @@ import (
 	"errors"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/core/types"
 )
 
 // NotFound is returned by API methods if the requested item does not exist.
