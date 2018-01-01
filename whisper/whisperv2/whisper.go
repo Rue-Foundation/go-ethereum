53bc55888a8d576bf143b6751e3dccbf4cd34388
rue-network <34838535+rue-network@users.noreply.github.com>
2018-01-01 11:39:16 -0500

    Update whisper.go

--- whisper/whisperv2/whisper.go
+++ whisper/whisperv2/whisper.go
@@ -22,13 +22,13 @@ import (
 	"sync"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/crypto/ecies"
-	"github.com/ethereum/go-ethereum/event/filter"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/p2p"
-	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/crypto"
+	"github.com/Rue-Foundation/go-rue/crypto/ecies"
+	"github.com/Rue-Foundation/go-rue/event/filter"
+	"github.com/Rue-Foundation/go-rue/log"
+	"github.com/Rue-Foundation/go-rue/p2p"
+	"github.com/Rue-Foundation/go-rue/rpc"
 
 	"gopkg.in/fatih/set.v0"
 )
