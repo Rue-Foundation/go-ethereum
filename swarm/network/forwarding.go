c379ab712eab7b087cb8f556082adbfc4fc5c35b
rue-network <34838535+rue-network@users.noreply.github.com>
2018-01-01 09:22:35 -0500

    Update forwarding.go

--- swarm/network/forwarding.go
+++ swarm/network/forwarding.go
@@ -21,8 +21,8 @@ import (
 	"math/rand"
 	"time"
 
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/swarm/storage"
+	"github.com/Rue-Foundation/go-rue/log"
+	"github.com/Rue-Foundation/go-rue/swarm/storage"
 )
 
 const requesterCount = 3
