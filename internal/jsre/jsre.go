c72c7f6542aecd5211e032d81f54874032178338
rue-network <34838535+rue-network@users.noreply.github.com>
2018-01-01 08:01:43 -0500

    Update jsre.go

--- internal/jsre/jsre.go
+++ internal/jsre/jsre.go
@@ -26,8 +26,8 @@ import (
 	"math/rand"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/internal/jsre/deps"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/internal/jsre/deps"
 	"github.com/robertkrimen/otto"
 )
 
