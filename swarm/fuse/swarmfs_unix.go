fae8cc4ad942ce5495f079af6ff0a979ac884d98
rue-network <34838535+rue-network@users.noreply.github.com>
2018-01-01 09:21:02 -0500

    Update swarmfs_unix.go

--- swarm/fuse/swarmfs_unix.go
+++ swarm/fuse/swarmfs_unix.go
@@ -29,9 +29,9 @@ import (
 
 	"bazil.org/fuse"
 	"bazil.org/fuse/fs"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/swarm/api"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/log"
+	"github.com/Rue-Foundation/go-rue/swarm/api"
 )
 
 var (
