9e72d027f01cf0ed419aea74929a8d17e9f2261c
rue-network <34838535+rue-network@users.noreply.github.com>
2018-01-01 08:23:24 -0500

    Update postprocess.go

--- light/postprocess.go
+++ light/postprocess.go
@@ -23,15 +23,15 @@ import (
 	"math/big"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/bitutil"
-	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/ethdb"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/params"
-	"github.com/ethereum/go-ethereum/rlp"
-	"github.com/ethereum/go-ethereum/trie"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/common/bitutil"
+	"github.com/Rue-Foundation/go-rue/core"
+	"github.com/Rue-Foundation/go-rue/core/types"
+	"github.com/Rue-Foundation/go-rue/ethdb"
+	"github.com/Rue-Foundation/go-rue/log"
+	"github.com/Rue-Foundation/go-rue/params"
+	"github.com/Rue-Foundation/go-rue/rlp"
+	"github.com/Rue-Foundation/go-rue/trie"
 )
 
 const (
