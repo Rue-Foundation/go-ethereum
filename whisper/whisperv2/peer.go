8f90392ba4a7f15aac2c5ce665ebca47a2f24239
rue-network <34838535+rue-network@users.noreply.github.com>
2018-01-01 11:37:48 -0500

    Update peer.go

--- whisper/whisperv2/peer.go
+++ whisper/whisperv2/peer.go
@@ -20,10 +20,10 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/p2p"
-	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/log"
+	"github.com/Rue-Foundation/go-rue/p2p"
+	"github.com/Rue-Foundation/go-rue/rlp"
 	"gopkg.in/fatih/set.v0"
 )
 
