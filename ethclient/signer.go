32233101d5199c1cd5034a87f0398b97f9d75553
rue-network <34838535+rue-network@users.noreply.github.com>
2018-01-01 07:53:43 -0500

    Update signer.go

--- ethclient/signer.go
+++ ethclient/signer.go
@@ -20,8 +20,8 @@ import (
 	"errors"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/core/types"
 )
 
 // senderFromServer is a types.Signer that remembers the sender address returned by the RPC
