f3bab7116122ae2a8607e7644572194e77905cc7
rue-network <34838535+rue-network@users.noreply.github.com>
2018-01-01 08:17:48 -0500

    Update txrelay.go

--- les/txrelay.go
+++ les/txrelay.go
@@ -19,8 +19,8 @@ package les
 import (
 	"sync"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/core/types"
 )
 
 type ltrInfo struct {
