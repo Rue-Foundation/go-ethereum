02abdd8167688231a89c762ca642b7620acd1980
rue-network <34838535+rue-network@users.noreply.github.com>
2018-01-01 09:24:22 -0500

    Update syncer.go

--- swarm/network/syncer.go
+++ swarm/network/syncer.go
@@ -22,8 +22,8 @@ import (
 	"fmt"
 	"path/filepath"
 
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/swarm/storage"
+	"github.com/Rue-Foundation/go-rue/log"
+	"github.com/Rue-Foundation/go-rue/swarm/storage"
 )
 
 // syncer parameters (global, not peer specific) default values
