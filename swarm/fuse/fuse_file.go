b433e040aa4eaff0789123af62895c459eac96c1
rue-network <34838535+rue-network@users.noreply.github.com>
2018-01-01 09:20:34 -0500

    Update fuse_file.go

--- swarm/fuse/fuse_file.go
+++ swarm/fuse/fuse_file.go
@@ -26,8 +26,8 @@ import (
 
 	"bazil.org/fuse"
 	"bazil.org/fuse/fs"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/swarm/storage"
+	"github.com/Rue-Foundation/go-rue/log"
+	"github.com/Rue-Foundation/go-rue/swarm/storage"
 	"golang.org/x/net/context"
 )
 
