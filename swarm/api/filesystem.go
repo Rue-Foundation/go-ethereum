3dfd34f78f4704daa626cf44d0b1d3c46548bcbd
rue-network <34838535+rue-network@users.noreply.github.com>
2018-01-01 09:16:51 -0500

    Update filesystem.go

--- swarm/api/filesystem.go
+++ swarm/api/filesystem.go
@@ -26,9 +26,9 @@ import (
 	"path/filepath"
 	"sync"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/swarm/storage"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/log"
+	"github.com/Rue-Foundation/go-rue/swarm/storage"
 )
 
 const maxParallelFiles = 5
