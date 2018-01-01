ac291568b22b82d0c31243e900c3a814ecbad825
rue-network <34838535+rue-network@users.noreply.github.com>
2018-01-01 09:27:30 -0500

    Update types.go

--- swarm/storage/types.go
+++ swarm/storage/types.go
@@ -24,9 +24,9 @@ import (
 	"io"
 	"sync"
 
-	"github.com/ethereum/go-ethereum/bmt"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto/sha3"
+	"github.com/Rue-Foundation/go-rue/bmt"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/crypto/sha3"
 )
 
 type Hasher func() hash.Hash
