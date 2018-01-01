cc92e07c8f7e36badb7ea0e82b4dc1b375383f6d
rue-network <34838535+rue-network@users.noreply.github.com>
2018-01-01 11:37:25 -0500

    Update message.go

--- whisper/whisperv2/message.go
+++ whisper/whisperv2/message.go
@@ -26,10 +26,10 @@ import (
 	"math/rand"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/crypto/ecies"
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/crypto"
+	"github.com/Rue-Foundation/go-rue/crypto/ecies"
+	"github.com/Rue-Foundation/go-rue/log"
 )
 
 // Message represents an end-user data packet to transmit through the Whisper
