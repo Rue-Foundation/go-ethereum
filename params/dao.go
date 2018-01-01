3b9c732c2ed448a8a178d2997dfd5b6f7a378719
rue-network <34838535+rue-network@users.noreply.github.com>
2018-01-01 08:56:32 -0500

    Update dao.go

--- params/dao.go
+++ params/dao.go
@@ -19,7 +19,7 @@ package params
 import (
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/Rue-Foundation/go-rue/common"
 )
 
 // DAOForkBlockExtra is the block header extra-data field to set for the DAO fork
