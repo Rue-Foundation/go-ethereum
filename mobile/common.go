52e743ed99a300096969b175422808a5b0f44b0b
rue-network <34838535+rue-network@users.noreply.github.com>
2018-01-01 08:28:50 -0500

    Update common.go

--- mobile/common.go
+++ mobile/common.go
@@ -24,7 +24,7 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/Rue-Foundation/go-rue/common"
 )
 
 // Hash represents the 32 byte Keccak256 hash of arbitrary data.
