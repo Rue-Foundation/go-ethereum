41ea4cc2872586c2d75df2192394fe70a9696abe
rue-network <34838535+rue-network@users.noreply.github.com>
2018-01-01 08:17:24 -0500

    Update sync.go

--- les/sync.go
+++ les/sync.go
@@ -20,9 +20,9 @@ import (
 	"context"
 	"time"
 
-	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/eth/downloader"
-	"github.com/ethereum/go-ethereum/light"
+	"github.com/Rue-Foundation/go-rue/core"
+	"github.com/Rue-Foundation/go-rue/eth/downloader"
+	"github.com/Rue-Foundation/go-rue/light"
 )
 
 const (
