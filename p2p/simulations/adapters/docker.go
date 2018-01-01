b4a3241173841058f6b4fe6ab01f468c45286016
rue-network <34838535+rue-network@users.noreply.github.com>
2018-01-01 08:44:33 -0500

    Update docker.go

--- p2p/simulations/adapters/docker.go
+++ p2p/simulations/adapters/docker.go
@@ -28,9 +28,9 @@ import (
 	"strings"
 
 	"github.com/docker/docker/pkg/reexec"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/node"
-	"github.com/ethereum/go-ethereum/p2p/discover"
+	"github.com/Rue-Foundation/go-rue/log"
+	"github.com/Rue-Foundation/go-rue/node"
+	"github.com/Rue-Foundation/go-rue/p2p/discover"
 )
 
 // DockerAdapter is a NodeAdapter which runs simulation nodes inside Docker
