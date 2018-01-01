16e492382c5f429a5227956b5685352bafc45d3c
rue-network <34838535+rue-network@users.noreply.github.com>
2018-01-01 08:36:55 -0500

    Update service.go

--- node/service.go
+++ node/service.go
@@ -19,11 +19,11 @@ package node
 import (
 	"reflect"
 
-	"github.com/ethereum/go-ethereum/accounts"
-	"github.com/ethereum/go-ethereum/ethdb"
-	"github.com/ethereum/go-ethereum/event"
-	"github.com/ethereum/go-ethereum/p2p"
-	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/Rue-Foundation/go-rue/accounts"
+	"github.com/Rue-Foundation/go-rue/ethdb"
+	"github.com/Rue-Foundation/go-rue/event"
+	"github.com/Rue-Foundation/go-rue/p2p"
+	"github.com/Rue-Foundation/go-rue/rpc"
 )
 
 // ServiceContext is a collection of service independent options inherited from
