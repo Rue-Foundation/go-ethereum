b8267162b3c8c64ac88e1e0d1b05b89744fcb073
rue-network <34838535+rue-network@users.noreply.github.com>
2018-01-01 09:17:12 -0500

    Update manifest.go

--- swarm/api/manifest.go
+++ swarm/api/manifest.go
@@ -27,9 +27,9 @@ import (
 	"sync"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/swarm/storage"
+	"github.com/Rue-Foundation/go-rue/common"
+	"github.com/Rue-Foundation/go-rue/log"
+	"github.com/Rue-Foundation/go-rue/swarm/storage"
 )
 
 const (
